fb1743eaf0cba6695098407d70d2b2d3f0b07af9
Kevin Csiffary <kevin.csiffary@stu.ocsb.ca>
2024-02-09 03:39:50 +0000

    Created HelloWorldStyle.swift

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Kevin Csiffary
+//  Created on 2024-02-08
+//  Version 1.0
+//  Copyright (c) 2024 Kevin Csiffary. All rights reserved.
+//
+//  Its Hello World, but with style.
+
+// Display Hello World! to the console
+print("Hello World!")
